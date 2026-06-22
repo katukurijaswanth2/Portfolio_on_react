0bad23f851559321d62bceeea1c556dfd370afe9
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-06-22 16:40:21 +0530

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <About />
       <GrowthCurve />
       <Skills />
-      <ProjectsSection />
+      {/* <ProjectsSection /> */}
       <RolesSection />
       <FullStack />
       <FrontEnd />
